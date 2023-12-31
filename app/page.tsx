7ed1a01b16674729a07bde012a33189a482bb138
DevMirza <pzhafeez@gmail.com>
2023-12-31 18:37:10 +0000

    add Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="p-6">
       <div className="mb-4">
         <h1 className="text-2xl font-semibold">Next Auth v5</h1>
         <p>In this tutorial from Antonio i am Learning Next Auth v5</p>
+        <Button variant="default">Just a Button!</Button>
       </div>
     </div>
   );
